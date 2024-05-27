9cb560a33c6e8bf98582567a724777e1b8ae66d8
Nada <nada@maitech.ps>
2024-05-27 17:47:37 +0300

    Update address controller request mapping

--- Inventory_Management_System_Rest_APIs/src/main/java/com/nadaabuissa/Inventory_Management_System_Rest_APIs/controller/AddressController.java
+++ Inventory_Management_System_Rest_APIs/src/main/java/com/nadaabuissa/Inventory_Management_System_Rest_APIs/controller/AddressController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/addresses")
+@RequestMapping("/addresses")
 public class AddressController {
 
     private final AddressService addressService;
